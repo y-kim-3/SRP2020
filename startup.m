fa7de062693cd3731328664e0027096a1cf6408a
y-kim-3 <67682355+y-kim-3@users.noreply.github.com>
2020-07-03 11:17:56 -0400

    Added folders to MATLAB

--- startup.m
+++ startup.m
@@ -1,5 +1,5 @@
 %put this file in the outermost level of your MATLAB working directory
 %set the working directory in Home => Environment => Preferences => General
 disp("SRP 2020 Stup")
-%adds all files in this folder and subfolders
-addpath(genpath('C:\Users\emily.jones\Documents\GitHub\SRP2020'));
+%adds all files in this folder and subfolders (genpath)
+addpath(genpath('C:\Users\yuria\OneDrive\Documents\GitHub\SRP2020'));
